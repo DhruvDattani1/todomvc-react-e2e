bba71108756baed971ab951d6a949a528eb05f85
Dhruv Dattani <dhruvd22@gmail.com>
2025-08-07 22:39:56 -0400

    updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://192.168.1.50:3000',
+    baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'cypress/support/e2e.js',
-    },
+  },
 });
